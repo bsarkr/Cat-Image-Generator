eeef8d831f4e9421d264d0352776dc3a0c8d9011
bsarkr <bsarkar@fordham.edu>
2025-02-04 11:27:07 -0500

    Initial Commit

---
+++ Cat-Image-Generator/Cat_Image_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_Image_GeneratorApp.swift
+//  Cat-Image-Generator
+//
+//  Created by Bilash Sarkar on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Cat_Image_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cat-Image-GeneratorTests/Cat_Image_GeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_Image_GeneratorTests.swift
+//  Cat-Image-GeneratorTests
+//
+//  Created by Bilash Sarkar on 2/4/25.
+//
+
+import Testing
+@testable import Cat_Image_Generator
+
+struct Cat_Image_GeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
